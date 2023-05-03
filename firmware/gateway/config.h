87cddcedf12ff6cbf12c44b7ae24a3cef359187e
Olocool17 <22843298+Olocool17@users.noreply.github.com>
2023-05-04 01:42:16 +0200

    Change pin assignemnts to reflect sensor board

--- firmware/gateway/config.h
+++ firmware/gateway/config.h
@@ -8,48 +8,47 @@
 
 // Pin assignments
 
-#define BOOT_PIN 1
+#define BOOT_PIN 0
 
 #define SDA_PIN 21
 #define SCL_PIN 22
 
 // LoRa module pins
-#define CS_PIN 13   // Chip select pin
-#define RST_PIN 15  // Reset pin
+#define CS_PIN 27   // Chip select pin
+#define RST_PIN 26  // Reset pin
 #define DIO0_PIN 34 // DIO0 pin: LoRa interrupt pin
-#define DIO1_PIN 4  // DIO1 pin: not used??
-#define TX_PIN 17
-#define RX_PIN 16
+#define TX_PIN 13
+#define RX_PIN 25
 
 // PCF2129 timer
-#define RTC_INT_PIN 35   // Interrupt pin
+#define RTC_INT_PIN 4    // Interrupt pin
 #define RTC_ADDRESS 0x51 // i2c address
 
-//Filepaths
+// Filepaths
 #define LOG_FP "/"
 #define NODES_FP "/nodes.dat"
 #define DATA_FP "/data.dat"
 
 // WiFi settings
-#define WIFI_SSID "GontrodeWiFi2"
+#define WIFI_SSID "PUT WIFI NAME HERE"
 #define WIFI_PASS "b5uJeswA"
 #define NTP_URL "be.pool.ntp.org"
 // MQTT settings
 #define MQTT_SERVER IPAddress(193, 190, 127, 143)
 #define MQTT_PORT 1883
 #define TOPIC_PREFIX "fornalab" // MQTT topic = `TOPIC_PREFIX` + '/' + `GATEWAY MAC` + '/' + `SENSOR MODULE MAC`
-//UART settings
-#define UART_PHASE_ENTRY_PERIOD 15 //s, length of time after wakeup and comm period in which command phase can be entered
-#define UART_PHASE_TIMEOUT (5 * 60) //s, length of UART inactivity required to automatically exit command phase
+// UART settings
+#define UART_PHASE_ENTRY_PERIOD 15  // s, length of time after wakeup and comm period in which command phase can be entered
+#define UART_PHASE_TIMEOUT (5 * 60) // s, length of UART inactivity required to automatically exit command phase
 
 // Communication and sensor settings
 #define COMMUNICATION_PERIOD_LENGTH ((SENSOR_DATA_TIMEOUT + TIME_CONFIG_TIMEOUT) / 1000) // s, time reserved for communication between gateway and one node
 #define COMMUNICATION_PERIOD_PADDING 2                                                   // s, time between each node's communication period
 #define COMMUNICATION_INTERVAL 60 * 60 * 24                                              // s, time between communication times for every nodes
 
-#define WAKE_BEFORE_COMM_PERIOD 20 //s, time before comm period when gateway should wake from deep sleep
-#define WAKE_COMM_PERIOD(X) ((X) - WAKE_BEFORE_COMM_PERIOD)
-#define LISTEN_COMM_PERIOD(X) ((X) - COMMUNICATION_PERIOD_PADDING)
+#define WAKE_BEFORE_COMM_PERIOD 20 // s, time before comm period when gateway should wake from deep sleep
+#define WAKE_COMM_PERIOD(X) ((X)-WAKE_BEFORE_COMM_PERIOD)
+#define LISTEN_COMM_PERIOD(X) ((X)-COMMUNICATION_PERIOD_PADDING)
 
 #define UPLOAD_EVERY 3 // amount of times the gateway will communicate with the nodes before uploading data to the server
 
