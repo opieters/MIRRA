1b9c4b8eac2e6432722008cc01882138dc8edce2
Olocool17 <22843298+Olocool17@users.noreply.github.com>
2023-05-20 19:29:07 +0200

    Move single-value sensors to Sensor lib

--- firmware/lib/Sensor/LightSensor.cpp
+++ firmware/lib/Sensor/LightSensor.cpp
@@ -1,31 +1,36 @@
 #include "LightSensor.h"
 
-LightSensor::LightSensor() {
+LightSensor::LightSensor()
+{
     this->baseSensor = AsyncAPDS9306();
 }
 
-void LightSensor::setup(void) {
+void LightSensor::setup(void)
+{
     baseSensor.begin(APDS9306_ALS_GAIN_1, APDS9306_ALS_MEAS_RES_16BIT_25MS);
 }
 
-void LightSensor::start_measurement(){
+void LightSensor::start_measurement()
+{
     baseSensor.startLuminosityMeasurement();
 }
 
-uint8_t LightSensor::read_measurement(float* data, uint8_t length) {
-    if(length < 1){
+uint8_t LightSensor::read_measurement(float *data, uint8_t length)
+{
+    if (length < 1)
+    {
         return 0;
     }
 
-    while(!baseSensor.isMeasurementReady());
+    while (!baseSensor.isMeasurementReady())
+        ;
 
     AsyncAPDS9306Data sensorData = baseSensor.getLuminosityMeasurement();
-
     *data = float(sensorData.raw);
 
     return 1;
 }
 
-LightSensor::~LightSensor() {
-    
+LightSensor::~LightSensor()
+{
 }
