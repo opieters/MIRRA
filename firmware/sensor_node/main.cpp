de97bdca0618a90f4e94de6d7b08ca6cf646b2f7
Olocool17 <22843298+Olocool17@users.noreply.github.com>
2023-07-25 19:22:39 +0200

    Make pins constexpr

--- firmware/sensor_node/main.cpp
+++ firmware/sensor_node/main.cpp
@@ -3,16 +3,16 @@
 
 void setup(void)
 {
-    const MIRRAModule::MIRRAPins pins = {.boot_pin = BOOT_PIN,
-                                         .sda_pin = SDA_PIN,
-                                         .scl_pin = SCL_PIN,
-                                         .cs_pin = CS_PIN,
-                                         .rst_pin = RST_PIN,
-                                         .dio0_pin = DIO0_PIN,
-                                         .tx_pin = TX_PIN,
-                                         .rx_pin = RX_PIN,
-                                         .rtc_int_pin = RTC_INT_PIN,
-                                         .rtc_address = RTC_ADDRESS};
+    constexpr MIRRAModule::MIRRAPins pins = {.boot_pin = BOOT_PIN,
+                                             .sda_pin = SDA_PIN,
+                                             .scl_pin = SCL_PIN,
+                                             .cs_pin = CS_PIN,
+                                             .rst_pin = RST_PIN,
+                                             .dio0_pin = DIO0_PIN,
+                                             .tx_pin = TX_PIN,
+                                             .rx_pin = RX_PIN,
+                                             .rtc_int_pin = RTC_INT_PIN,
+                                             .rtc_address = RTC_ADDRESS};
     MIRRAModule::prepare(pins);
     SensorNode sensorNode = SensorNode(pins);
     sensorNode.wake();
